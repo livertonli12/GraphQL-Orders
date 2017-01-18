1f4fc2165bc859c20e8bbc6c16c85869fa292fff
Líverton Felipe de Gois Oliveira <liverton.oliveira@fcamara.com.br>
2017-01-18 19:19:13 -0200

    cors

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ process.env.PORT     = process.env.PORT || 8080;
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 import express from 'express';
+import cors from 'cors';
 import graphqlHTTP from 'express-graphql';
 import mongoose from 'mongoose';
 
 import schema from './graphql';
 
 var app = express();
+
+app.use(cors());
 
 app.use('/api/orders', graphqlHTTP({
     schema,
