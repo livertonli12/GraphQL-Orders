f6caadf8c2b5e7107d2f24d30c82fe8286c14163
Líverton Felipe de Gois Oliveira <liverton.oliveira@fcamara.com.br>
2017-01-18 18:38:50 -0200

    Changing port

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-process.env.PORT     = process.env.PORT || 8080;
+process.env.PORT     = process.env.PORT || 80;
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 import express from 'express';
